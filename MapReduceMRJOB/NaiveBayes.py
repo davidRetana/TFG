8385bfec78241db744fd4f2782acfaab7e32fc2f
david <dretana@corenetworks.es>
2017-08-23 09:05:46 +0200

    Algoritmo NaiveBayes distribuido

---
+++ MapReduceMRJOB/NaiveBayes.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 08 10:21:02 2017
+
+@author: David Retana
+
+Compute naiveBayes in a parallelized way
+"""
+
+from __future__ import print_function, division
+from mrjob.job import MRJob
+import sys
+
+
+class NaiveBayes(MRJob):
+    
+    regex = "," # separador de campos
+
+    def mapper(self, _, line):
+       fields = line.split(self.regex)
+       x = fields[:-1] # caracteristicas
+       y = fields[-1] # clase o etiqueta
+
+       for i in range(len(x)):
+           yield((float(y), i), (float(x[i]), 1))
+
+    def reducer(self, key, values): 
+       m = 0.0 # numero de registros
+       sum_features = 0.0
+       sum_features_squared = 0.0
+       for feature, i in values:
+           sum_features += feature
+           sum_features_squared += feature**2
+           m += i
+       muj = sum_features / m
+       sj2 = (sum_features_squared + m*muj**2 -2*muj*sum_features) / m
+       yield (key, (muj, sj2))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage naiveBayes: <input_file>', file=sys.stderr)
+        exit(-1)
+    print('Starting parallelized NaiveBayes computation')
+    path = sys.argv[1]
+    job = NaiveBayes(args=[path])
+    runner = job.make_runner()
+    runner.run()
+    tmp_output = []
+    for line in runner.stream_output():
+        tmp_output.append(line.split("\t"))
+    for i in tmp_output:
+        print('element: ', i)
